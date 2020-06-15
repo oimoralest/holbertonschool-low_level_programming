fa653b6a78b5b0d2cbeca40e74360c4998b1f496
oimoralest <oimoralest@gmail.com>
2020-06-15 14:48:44 -0500

    added

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *main - entry
+ *
+ *Description: description
+ *
+ *Return: 0 (success)
+ */
+int main(void)
+{
+	char c[9] = "Holberton";
+	int i;
+
+	for (i = 0; i < 9; ++i)
+		_putchar(c[i]);
+	_putchar('\n');
+	return (0);
+}
