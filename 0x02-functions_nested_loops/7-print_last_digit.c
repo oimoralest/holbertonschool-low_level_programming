659fc9ff93eade83cdb476629fcf7ffea737c697
oimoralest <oimoralest@gmail.com>
2020-06-16 10:41:56 -0500

    fixed the use of n as the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,13 +3,15 @@
  *print_last_digit - prints the last digits of a number
  *@n: the digit to print its last digit
  *
- *Return: n the value of the last digit
+ *Return: ld the value of the last digit
  */
 int print_last_digit(int n)
 {
+	int ld;
+
 	if (n < 0)
-		n = n * -1;
-	n = n % 10;
-	_putchar(n + '0');
-	return (n);
+		n *= -1;
+	ld = n % 10;
+	_putchar(ld + '0');
+	return (ld);
 }
