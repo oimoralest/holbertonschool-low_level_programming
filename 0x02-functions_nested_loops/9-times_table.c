c8b5d4cd94cfc5902a4833b5d77f77802aaa0e30
oimoralest <oimoralest@gmail.com>
2020-06-16 11:19:12 -0500

    fixed output format

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,11 +12,11 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; ++j)
 		{
-			if ((j * i) >= 10)
-				_putchar(((j * i) / 10) + '0');
-			else
+			if ((i * j) >= 10)
+				_putchar(((i * j) / 10) + '0');
+			else if (j > 0)
 				_putchar(' ');
-			_putchar(((j * i) % 10) + '0');
+			_putchar(((i * j) % 10) + '0');
 			if (j != 9)
 			{
 				_putchar(',');
