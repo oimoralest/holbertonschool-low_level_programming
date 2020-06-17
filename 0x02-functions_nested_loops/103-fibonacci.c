a38490054a9ec8bd8c68363e2422af9285ca46c1
oimoralest <oimoralest@gmail.com>
2020-06-17 10:37:53 -0500

    fixed to included the number 2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -12,13 +12,13 @@ int main(void)
 	i = 1;
 	j = 2;
 	sum_0 = sum_1 = 0;
-	while (sum_0 <= 4000000)
+	while (j < 4000000)
 	{
+		if ((j % 2) == 0)
+			sum_1 = sum_1 + j;
 		sum_0 = i + j;
 		i = j;
 		j = sum_0;
-		if ((sum_0 % 2) == 0)
-			sum_1 = sum_1 + sum_0;
 	}
 	printf("Sum of the even_valued terms is: %i\n", sum_1);
 	return (0);
