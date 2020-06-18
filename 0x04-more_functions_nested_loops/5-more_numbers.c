64ac9f793af405e4379f0aabbd182a953fbf95d4
oimoralest <oimoralest@gmail.com>
2020-06-18 13:37:41 -0500

    fixed the condition to print double digit numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,14 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; ++i)
 	{
-		for (j = 0; j <= 9; ++j)
-			_putchar(j + '0');
-		for (j = 0; j <= 4; ++j)
-		{
-			_putchar('1');
-			_putchar(j + '0');
-		}
+		for (j = 0; j <= 14; ++j)
+			if (j <= 9)
+				_putchar(j + '0');
+			else
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
 		_putchar('\n');
 	}
 }
