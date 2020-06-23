e40ed10a53bef70665af219826b684cab28b0e65
oimoralest <oimoralest@gmail.com>
2020-06-23 16:04:55 -0500

    modified

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,9 @@
  */
 void puts2(char *str)
 {
-	int c = 0;
-
-	while (str[c] != '\0')
-	{
-		_putchar(str[c]);
-		c += 2;
-	}
+	int i;
+	
+	for (i = 0; str[i] != '\0'; i += 2)
+		_putchar(str[i]);
 	_putchar('\n');
 }
