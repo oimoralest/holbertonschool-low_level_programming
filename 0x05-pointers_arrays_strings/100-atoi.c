c74a5dfd7234eeab0ff660a2a5704726938ee921
oimoralest <oimoralest@gmail.com>
2020-06-23 18:09:47 -0500

    fixed second else if condition

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,7 +15,7 @@ int _atoi(char *s)
 			sign++;
 		else if (s[i] >= '0' && s[i] <= '9')
 			num = num * 10 + (s[i] - '0');
-		else if (num != 0)
+		else if (num > 0)
 			break;
 	if (sign % 2 == 0)
 		return (num);
