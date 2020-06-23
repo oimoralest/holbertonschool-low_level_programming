4094f735f97043e88da15cb79af866f780ab5a2a
oimoralest <oimoralest@gmail.com>
2020-06-23 15:22:36 -0500

    fixed the condition for the second while

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,14 +11,14 @@ void rev_string(char *s)
 	int len, c, c1;
 
 	len = c = 0;
-	while ((temp = *(s + len)) != '\0')
+	while ((temp = s[len]) != '\0')
 		len++;
 	c1 = len - 1;
-	while (c <= len / 2)
+	while (c <= c1)
 	{
-		temp = *(s + c);
-		*(s + c) = *(s + c1);
-		*(s + c1) = temp;
+		temp = s[c];
+		s[c] = s[c1];
+		s[c1] = temp;
 		c1--;
 		c++;
 	}
