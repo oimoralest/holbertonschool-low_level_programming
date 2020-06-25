7cd36bd963f64430666154941cac72fef52b3352
oimoralest <oimoralest@gmail.com>
2020-06-25 12:37:41 -0500

    modified to work exactly like strcmp function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,22 +5,19 @@
  *@s1: first string to compare with...
  *@s2: second string
  *
- *Return: 15 if s1 is greater than s2
+ *Return: (*s1 - *s2) the difference between both strings
  *         0 if both are equal
- *       -15 if s1 is smaller than s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != '\0' && *s2 != '\0')
 	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
 		s1++;
 		s2++;
 	}
-	if (*s1 == '\0' && *s2 != '\0')
-		return (-15);
-	else if (*s1 == '\0' && *s2 == '\0')
-		return (0);
-	else
-		return (15);
+
+	return (0);
 }
