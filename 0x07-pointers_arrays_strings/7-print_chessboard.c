6f1def472af6f4f7f64e01621593a3a462c2470a
oimoralest <oimoralest@gamil.com>
2020-06-29 18:52:21 +0000

    added

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *print_chessboard - print the chessboard
+ *@a: multi-dimensional array with the characters to print
+ *
+ *Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
