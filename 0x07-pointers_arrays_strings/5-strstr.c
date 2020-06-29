d5bba9a124769e2a4f9750cdf8659dfb5881879f
oimoralest <oimoralest@gamil.com>
2020-06-29 18:19:19 +0000

    added

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+ *_strstr - locates a substring
+ *@haystack: string to search within it
+ *@needle: substring to search in @haystack
+ *
+ *Return: ip (initial position where the substring is founded)
+ *        NULL if the substring is not founded
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	char *ip;
+
+	while (*haystack != '\0')
+	{
+		if (*haystack == needle[i])
+		{
+			ip = haystack;
+			while (*haystack == needle[i] && needle[i] != '\0')
+			{
+				haystack++;
+				i++;
+			}
+			if (needle[i] == '\0')
+				goto exit;
+			else
+			{
+				haystack++;
+				i = 0;
+				continue;
+			}
+		}
+		else
+		{
+			haystack++;
+			i = 0;
+		}
+	}
+
+exit: return (needle[i] == '\0' ? ip : '\0');
+}
