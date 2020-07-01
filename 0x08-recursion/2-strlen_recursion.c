e74e21127a79b6700e417d9f501b08c73de76ebc
oimoralest <oimoralest@gamil.com>
2020-07-01 18:40:07 +0000

    added

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *returns the length of a string
+ *@s: pointer to the string
+ *
+ *Return: len length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s == '\0')
+		return (0);
+	len = _strlen_recursion(s + 1) + 1;
+
+	return (len);
+}
