fadbf04c21c1b675b5a7c09f54540a181dfb92bd
oimoralest <oimoralest@gamil.com>
2020-07-01 23:12:04 +0000

    modified

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -11,5 +11,8 @@ int _sqrt_recursion(int n);
 int _div(int i, int n);
 int is_prime_number(int n);
 int is_prime(int i, int n);
+int is_palindrome(char *s);
+char *final_position(char *s);
+int palindrome(char *s, char *fp);
 
 #endif
