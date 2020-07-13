5164228297df7f0543e908049afdf15b71ac0422
oimoralest <oimoralest@gmail.com>
2020-07-13 17:10:33 -0500

    added

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ *free_dog - frees dogs
+ *@d: pointer to the structure
+ *
+ *Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d-> owner);
+		free(d);
+	}
+}
