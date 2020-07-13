298a54d53071f007dec17ca8fdd5eac38a3e9b4e
oimoralest <oimoralest@gmail.com>
2020-07-13 16:30:13 -0500

    modified

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,5 +24,7 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+int str_len(char *str);
+void str_cp(char *cp, char *str);
 
 #endif
