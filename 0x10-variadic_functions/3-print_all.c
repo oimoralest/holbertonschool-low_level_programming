8136b6fa3b97502bd05c665db2f5aedd393a9fdd
oimoralest <oimoralest@gmail.com>
2020-07-19 14:03:04 -0500

    modified to take into account NULL format

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -26,38 +26,41 @@ void print_all(const char * const format, ...)
 	char *ptr = NULL;
 
 	len = _strlen(format);
-	va_start(_print, format);
-	while (format[i])
+	if (len != 0)
 	{
-		separator = 1;
-		switch (format[i])
+		va_start(_print, format);
+		while (format[i])
 		{
-		case 'c':
-			printf("%c", va_arg(_print, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(_print, double));
-			break;
-		case 'i':
-			printf("%i", va_arg(_print, int));
-			break;
-		case 's':
-			ptr = va_arg(_print, char *);
-			if (ptr == NULL)
+			separator = 1;
+			switch (format[i])
 			{
-				printf("(nil)");
+			case 'c':
+				printf("%c", va_arg(_print, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(_print, double));
+				break;
+			case 'i':
+				printf("%i", va_arg(_print, int));
+				break;
+			case 's':
+				ptr = va_arg(_print, char *);
+				if (ptr == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", ptr);
+				break;
+			default:
+				separator = 0;
 				break;
 			}
-			printf("%s", ptr);
-			break;
-		default:
-			separator = 0;
-			break;
+			if (i < len - 1 && separator)
+				printf(", ");
+			i++;
 		}
-		if (i < len - 1 && separator)
-			printf(", ");
-		i++;
+		printf("\n");
+		va_end(_print);
 	}
-	printf("\n");
-	va_end(_print);
 }
