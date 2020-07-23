e36d67e890edc8075a6565ffea6f791e04ae7afa
oimoralest <oimoralest@gmail.com>
2020-07-23 14:53:36 -0500

    added

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ *str_len - returns the length of a string
+ *@str: string
+ *
+ *Return: i length of the @str
+ */
+int str_len(const char *str)
+{
+	int i = 0;
+
+	while (str[++i])
+		;
+
+	return (i);
+}
+/**
+ *add_node - adds a new node at the beginning of a list
+ *@head: head of the list
+ *@str: data for every node
+ *
+ *Return: NULL if malloc fails
+ *new_node address of the new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = str_len(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
