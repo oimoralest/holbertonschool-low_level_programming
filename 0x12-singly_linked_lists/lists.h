395c6eec385c99121f313841022282653924639d
oimoralest <oimoralest@gmail.com>
2020-07-23 15:38:31 -0500

    added prototype for add_node_end function

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,5 +23,6 @@ int _putchar(char c);
 size_t list_len(const list_t *h);
 int str_len(const char *);
 list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif /* lists.h */
