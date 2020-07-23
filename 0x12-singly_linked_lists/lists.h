af61107c0b8c692ac0ca5479d4db20db9c930564
oimoralest <oimoralest@gmail.com>
2020-07-23 14:54:29 -0500

    added prototype for str_len and add_node functions

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define _LISTS_H_
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  *struct list_s - singly linked list
  *@str: string - (malloc'ed string)
@@ -20,5 +21,7 @@ typedef struct list_s
 size_t print_list(const list_t *h);
 int _putchar(char c);
 size_t list_len(const list_t *h);
+int str_len(const char *);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /* lists.h */
