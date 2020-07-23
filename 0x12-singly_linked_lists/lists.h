db16b709256ae56bedb2356f7f22a6fd533e8c41
oimoralest <oimoralest@gmail.com>
2020-07-23 16:07:28 -0500

    added prototype for before_main function

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,5 +25,6 @@ int str_len(const char *);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void __attribute__ ((constructor)) before_main();
 
 #endif /* lists.h */
