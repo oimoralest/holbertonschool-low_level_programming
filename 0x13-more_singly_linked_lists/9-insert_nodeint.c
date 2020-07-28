9b36440ad38427902a507bd331a24f5ba450275e
oimoralest <oimoralest@gmail.com>
2020-07-28 11:21:48 -0500

    Added if statement line 17 and 28, and modified the return line 40, 42, and 45

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,22 +11,36 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	listint_t *new_node = NULL;
+	listint_t *new_node;
 	listint_t *aux;
 
-	aux = *head;
-	while (aux && i < idx)
-		aux = aux->next, i++;
-	if (aux && i == idx)
+	if (head)
 	{
-		new_node = malloc(sizeof(listint_t));
-		if (!new_node)
-			return (NULL);
-		new_node->n = n, i = 0, aux = *head;
-		while (i < idx - 1)
+		aux = *head;
+		while (aux && i < idx)
 			aux = aux->next, i++;
-		new_node->next = aux->next, aux->next = new_node;
+		if (aux && i == idx)
+		{
+			new_node = malloc(sizeof(listint_t));
+			if (!new_node)
+				return (NULL);
+			new_node->n = n, i = 0, aux = *head;
+			if (!idx)
+			{
+				aux = aux->next;
+				new_node->next = aux;
+				*head = new_node;
+			}
+			else
+			{
+				while (i < idx - 1)
+					aux = aux->next, i++;
+				new_node->next = aux->next, aux->next = new_node;
+			}
+			return (new_node);
+		}
+		return (NULL);
 	}
 
-	return (new_node);
+	return (NULL);
 }
