3324c96d185d5d8b0db71bbcefbca3152759667f
oimoralest <oimoralest@gmail.com>
2020-08-04 15:49:31 -0500

    modified

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,9 @@ int create_file(const char *filename, char *text_content)
 	int file_descriptor = 0, i = 0;
 	ssize_t aux_write = 0;
 
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC,
+	if (!filename)
+                return (-1);
+        file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC,
 				S_IRUSR | S_IWUSR);
 	if (file_descriptor == -1)
 		return (-1);
