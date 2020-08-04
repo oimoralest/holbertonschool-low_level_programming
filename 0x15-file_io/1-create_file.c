a2401cf6b28b00bcb316c096e58a2af89de0efb6
oimoralest <oimoralest@gmail.com>
2020-08-04 15:44:03 -0500

    erased if statement in line 14

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,8 +11,6 @@ int create_file(const char *filename, char *text_content)
 	int file_descriptor = 0, i = 0;
 	ssize_t aux_write = 0;
 
-	if (!filename)
-		return (-1);
 	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC,
 				S_IRUSR | S_IWUSR);
 	if (file_descriptor == -1)
