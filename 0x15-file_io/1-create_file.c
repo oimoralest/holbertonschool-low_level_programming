94ba9c33da89513f2e523742f5b4e9698b570cac
oimoralest <oimoralest@gmail.com>
2020-08-04 15:36:54 -0500

    Added function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *create_file - creates a file
+ *@filename: file's name
+ *@text_content: content to copy to the created file
+ *
+ *Return: -1 if fails or 1 if success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor = 0, i = 0;
+	ssize_t aux_write = 0;
+
+	if (!filename)
+		return (-1);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC,
+				S_IRUSR | S_IWUSR);
+	if (file_descriptor == -1)
+		return (-1);
+	while (text_content[i] && text_content)
+	{
+		aux_write = write(file_descriptor, &text_content[i], 1);
+		if (aux_write == -1)
+			close(file_descriptor), return (-1);
+		i++;
+	}
+	close(file_descriptor);
+
+	return (1);
+}
