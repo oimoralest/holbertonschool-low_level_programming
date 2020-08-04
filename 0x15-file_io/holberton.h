aaedbfd16d5a4ec214fc99eafa48c498dd81b75e
oimoralest <oimoralest@gmail.com>
2020-08-04 15:03:34 -0500

    Added the header for the functions

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,9 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
