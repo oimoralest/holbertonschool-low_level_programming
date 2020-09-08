f2a8f73c7dc9c5415aa2e1c39ca9d06eb814b333
oimoralest <oimoralest@gmail.com>
2020-09-07 21:00:32 -0500

    Modified

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/oimoralest/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/make_me_win.so
+wget -P /tmp/ https://raw.githubusercontent.com/oimoralest/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/make_me_win.so
 export LD_PRELOAD=/tmp/make_me_win.so
