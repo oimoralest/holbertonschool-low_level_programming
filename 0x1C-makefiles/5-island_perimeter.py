f3352050d2c1307052f3dc88329ec76989a1b278
oimoralest <oimoralest@gmail.com>
2020-11-06 14:52:39 -0500

    5-island_perimeter.py: Added. Computes the perimeter of an island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This modules defines de island_perimeter function
+"""
+
+
+def island_perimeter(grid):
+    """Computes the perimeter of an island
+
+    Args:
+        grid (list[list[int]]): list of list of integer which 0 represents
+        water and 1 represents land
+
+    Returns:
+        [int]: perimeter of the island
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i == 0:
+                    perimeter += 1
+                elif grid[i - 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1:
+                    perimeter += 1
+                elif grid[i + 1][j] == 0:
+                    perimeter += 1
+    return perimeter
