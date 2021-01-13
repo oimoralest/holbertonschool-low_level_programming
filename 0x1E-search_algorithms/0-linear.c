efd76511ecf831ce535d0d9fd890c1e63485cd41
oimoralest <oimoralest@gmail.com>
2021-01-13 14:46:25 -0500

    Modified if statement

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,14 +11,15 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (!array)
-		return (-1);
-	while (i < size)
+	if (array)
 	{
-		if (array[i] == value)
-			return ((int)i);
-		printf("Value checked array[%li] = [%i]\n", i, array[i]);
-		i++;
+		while (i < size)
+		{
+			if (array[i] == value)
+				return (i);
+			printf("Value checked array[%li] = [%i]\n", i, array[i]);
+			i++;
+		}
 	}
 	return (-1);
 }
